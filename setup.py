44b593d65129722c9e09392b07d31601ddc4c617
mcolella14 <mcolella14@gmail.com>
2021-06-25 15:25:49 -0400

    publish

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+import pathlib
 import setuptools
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 setuptools.setup(
     name="pyppeteer_ghost_cursor",
-    version="0.1",
+    version="0.2.0",
     description="Pyppeteer implementation of Xetera/ghost-cursor",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/mcolella14/pyppeteer_ghost_cursor",
     author="mcolella14",
+    author_email="mcolella14@gmail.com",
+    license="MIT",
     packages=setuptools.find_packages(),
     package_data={"pyppeteer_ghost_cursor": ["js/*.js"]},
     install_requires=["bezier"],
